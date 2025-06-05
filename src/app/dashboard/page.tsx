641036157d25e30ecc8e8c47304312654718e9ef
Matheus Carvalho <matheusmarques150@hotmail.com>
2025-06-05 09:39:16 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -9,6 +12,14 @@ const Dashboard = async () => {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  //clinicas usuario
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
   return (
     <div>
